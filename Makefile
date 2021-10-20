3776d540ba7a0e6d77deb6b1fd9458c50327766c
Vicente Sepulveda <vicente.sepulveda.g@usach.cl>
2021-10-20 17:44:49 -0300

    Archivos

---
+++ Makefile
@@ -0,0 +1,29 @@
+CC= gcc
+CFLAGS= -Wall -lm -lniceprint
+out = lab1
+source = test.c Funciones.c
+objects = $(source:.c=.o)
+depends = .depends
+headers = Funciones.h
+
+$(build): $(out)
+$(out): $(objects)
+	$(CC) -o $@ $^ $(CFLAGS) -L .
+
+$(objects): 
+	$(CC) -c $*.c
+
+$(depends): $(source) $(headers)
+	@$(CC) -MM $(source) > $@
+
+clean:
+	$(RM) $(out) $(objects) $(depends)
+
+.PHONY: clean
+sinclude $(depends)
+
+run:
+	./lab1 -a 1 -b 2 -c hola
+	./lab1 -a 2 -b 5 -c hola
+	./lab1 -a 3 -b 4 -c hola
+	./lab1 -a 4 -b 4 -c hola
---
+++ lab1.c
@@ -0,0 +1,73 @@
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include "Funciones.h"
+
+void extern niceprint(int N, float*Energy);
+
+
+//Autores:     - Marcela Rivera 
+int main (int argc, char **argv) {
+
+  int numero_celdas = 0;
+  int c;
+  opterr = 0;
+
+//el siguiente ciclo se utiliza para recibir los parametros de entrada usando getopt
+  while ((c = getopt (argc, argv, "N:")) != -1)
+    printf("hola %s", c);
+    switch (c)
+      {
+      case 'N':
+        sscanf(optarg, "%d", numero_celdas);
+        printf("numero celdas: %d ", numero_celdas);
+        break;
+      /*case 'i':
+        //sscanf(optarg, "%d", &bvalue);
+        break; 
+      case 'o':
+        //cvalue = optarg;
+        break;*/
+      case '?':
+
+        /*if (optopt == 'c'){
+          fprintf (stderr, "Opcion -%c requiere un argumento.\n", optopt);
+        }
+        
+        else if (isprint (optopt))
+          fprintf (stderr, "Opcion desconocida `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Opcion con caracter desconocido `\\x%x'.\n",
+                   optopt);
+        return 1;
+      */
+      default:
+      printf("salida");
+        abort ();
+
+
+      }
+  /*printf ("avalue = %i, bvalue = %i, cvalue = %s\n",
+          avalue, bvalue, cvalue);
+ 
+//-----------------------------------------------------------------------------------------------------------------
+
+  hola();
+  
+  int N=10;
+  float* Energy = (float*)malloc(sizeof(float)*N);
+  srand((unsigned int)time(NULL));
+  for(int i = 0; i<N; i++){
+    Energy[i] = (float)rand()/(float)RAND_MAX;
+  }
+  niceprint(N,Energy);
+  */
+  return 0;
+}
+
+
