a5d759cf1210bf72c6a3063f048648bb3eb91992
matias <matias.jaque@usach.cl>
2021-10-21 11:59:57 -0300

    arreglos pequeños

--- lab1.c
+++ lab1.c
@@ -6,8 +6,7 @@
 #include <unistd.h>
 #include <time.h>
 #include "Funciones.h"
-
-void extern niceprint(int N, float*Energy);
+#include <math.h>
 
 
 //Autores:     - Marcela Rivera 
@@ -16,14 +15,15 @@ int main (int argc, char **argv) {
   int numero_celdas = 0;
   int c;
   opterr = 0;
+  printf("hola");
 
 //el siguiente ciclo se utiliza para recibir los parametros de entrada usando getopt
-  while ((c = getopt (argc, argv, "N:")) != -1)
-    printf("hola %s", c);
+  while ((c = getopt (argc, argv, "N:c")) != -1)
+    printf("hola");
     switch (c)
-      {
+    {
       case 'N':
-        sscanf(optarg, "%d", numero_celdas);
+        sscanf(optarg, "%d", &numero_celdas);
         printf("numero celdas: %d ", numero_celdas);
         break;
       /*case 'i':
@@ -34,7 +34,7 @@ int main (int argc, char **argv) {
         break;*/
       case '?':
 
-        /*if (optopt == 'c'){
+        if (optopt == 'c'){
           fprintf (stderr, "Opcion -%c requiere un argumento.\n", optopt);
         }
         
@@ -45,7 +45,7 @@ int main (int argc, char **argv) {
                    "Opcion con caracter desconocido `\\x%x'.\n",
                    optopt);
         return 1;
-      */
+      
       default:
       printf("salida");
         abort ();
